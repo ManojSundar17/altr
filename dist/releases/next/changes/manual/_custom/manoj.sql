e95b78546292d5d948293e2561a4dfe2c2950e06
Manoj <manoj.sundar@oracle.com>
2025-10-23 19:02:30 +0530

    Manual file added

---
+++ dist/releases/next/changes/manual/_custom/manoj.sql
@@ -0,0 +1,7 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1761226278586 stripComments:false logicalFilePath:manual/_custom/manoj.sql
+-- sqlcl_snapshot dist/releases/next/changes/manual/_custom/manoj.sql:null:null:custom
+
+
+
+insert into wksp_sqlcl.manoj_oct_22 (id) values (10);
