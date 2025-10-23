8476d4efa7f4994642c1fdaa15330df0f99e1690
Manoj <manoj.sundar@oracle.com>
2025-10-23 17:06:02 +0530

    Prod Dumps

---
+++ src/database/wksp_sqlcl/tables/prod_table.sql
@@ -0,0 +1,6 @@
+create table wksp_sqlcl.prod_table (
+    id number
+);
+
+
+-- sqlcl_snapshot {"hash":"52106026f538ad0eb687e89392d83b467efb2ede","type":"TABLE","name":"PROD_TABLE","schemaName":"WKSP_SQLCL","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>WKSP_SQLCL</SCHEMA>\n   <NAME>PROD_TABLE</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>ID</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n            \n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n      \n   </RELATIONAL_TABLE>\n</TABLE>"}
---
+++ src/database/wksp_sqlcl/tables/testing_dev.sql
@@ -0,0 +1,7 @@
+create table wksp_sqlcl.testing_dev (
+    id   number,
+    name varchar2(10 byte)
+);
+
+
+-- sqlcl_snapshot {"hash":"2643582cc2c9dc9469cc853ad8bc66f6e3e7bc8a","type":"TABLE","name":"TESTING_DEV","schemaName":"WKSP_SQLCL","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>WKSP_SQLCL</SCHEMA>\n   <NAME>TESTING_DEV</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>ID</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n            \n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>NAME</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>10</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n            \n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n      \n   </RELATIONAL_TABLE>\n</TABLE>"}
